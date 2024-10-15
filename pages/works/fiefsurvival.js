817e1af38be4ebd174a68323068edc7c4918345e
mrboat <mr.boat@gmx.com>
2024-10-15 18:31:46 +0300

    Update 0.5

--- pages/works/fiefsurvival.js
+++ pages/works/fiefsurvival.js
@@ -8,7 +8,6 @@ import {
   Box
 } from '@chakra-ui/react'
 import Layout from '../../components/layouts/article'
-import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 
